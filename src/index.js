646f5006c3d249448c31453cfa2f809666e4278b
ArcticFox85 <60521197+ArcticFox85@users.noreply.github.com>
2020-04-26 16:41:40 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lat: null,
+            errorMessage: ""
+        };
+        window.navigator.geolocation.getCurrentPosition(position => {
+                this.setState({
+                    lat: position.coords.latitude
+                });
+            },
+            err => {
+                this.setState({
+                    errorMessage: err.message
+                });
+
+            }
+        );
+
+    }
+
+
+
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div > Error: {
+                this.state.errorMessage
+            } < /div>
+
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div > Latitude: {
+                this.state.lat
+            } < /div>
+
+        }
+        return <div > Loading! < /div>
+
+
+
+    }
+
+
+}
+
+ReactDom.render( < App / > , document.querySelector('#root'))
